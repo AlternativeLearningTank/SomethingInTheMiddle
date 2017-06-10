2a5f52ea7aebeb23733ef8138d4f946d4f98911b
Luis Fernandez <dropmeaword@gmail.com>
2017-06-10 10:52:19 +0200

    woked a bit on the sniffer script

--- doc/PRACTICAL.md
+++ doc/PRACTICAL.md
@@ -1,1 +1,10 @@
 ### Practical setup
+
+### Notes
+
+Have a look at this method to run mitmproxy on the pi:
+https://hackaday.io/project/10338/instructions
+
+These are pentesting-ready distros for the Pi:
+http://pwnpi.sourceforge.net/ (seems very old)
+https://github.com/pwnieexpress/raspberry_pwn
--- src/httpsniff/sniff.py
+++ src/httpsniff/sniff.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-# from http://www.lucainvernizzi.net/blog/2015/02/12/extracting-urls-from-network-traffic-in-just-9-python-lines-with-scapy-http/
-# requires https://github.com/invernizzi/scapy-http
 from scapy.all import IP, sniff
 from scapy.layers import http
 from pprint import pprint
+import paho.mqtt.publish as publish
 
-def process_intercept(origin, dsthost, dstpath, dstmethod):
-    print "*"*69
-    print '\n{0} just requested a {1} {2}{3}'.format(origin, dstmethod, dsthost, dstpath)
 
-def process_tcp_packet(packet):
+MQTT_TOPIC="sitm/http"
+
+def handle_capture(origin, dsthost, dstpath, dstmethod):
+    msg = "\n{0} \t {1} {2}{3}".format(origin, dstmethod, dsthost, dstpath)
+    print msg
+    publish.single(topic=MQTT_TOPIC, payload=msg)
+
+def process_http_packet(packet):
     '''
     Processes a TCP packet, and if it contains an HTTP request, it prints it.
     '''
@@ -19,10 +22,6 @@ def process_tcp_packet(packet):
         return
     http_layer = packet.getlayer(http.HTTPRequest)
     ip_layer = packet.getlayer(IP)
-    #print "*"*69
-    #pprint(ip_layer.fields)
-    #pprint(http_layer.fields)
-    process_intercept(ip_layer.fields['src'], http_layer.fields['Host'], http_layer.fields['Path'], http_layer.fields['Method'])
+    handle_capture(ip_layer.fields['src'], http_layer.fields['Host'], http_layer.fields['Path'], http_layer.fields['Method'])
 
-# Start sniffing the network.
-sniff(iface='wlan0', filter='tcp port 80', prn=process_tcp_packet)
+sniff(iface='wlan0', filter='tcp port 80', prn=process_http_packet)
