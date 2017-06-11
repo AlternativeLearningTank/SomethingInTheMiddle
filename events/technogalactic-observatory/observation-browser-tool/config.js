dfd3e7662deb50ff7e521504634efc441099342f
Luis Fernandez <dropmeaword@gmail.com>
2017-06-11 22:41:07 +0200

    simples web client athat could possibly work, pretty ugly though

--- events/technogalactic-observatory/observation-browser-tool/config.js
+++ events/technogalactic-observatory/observation-browser-tool/config.js
@@ -1,2 +1,5 @@
-websocketserver = 'broker.mqttdashboard.com';
-websocketport = 8000;
+//wsbroker = '192.168.42.1';
+wsbroker='172.22.22.22';
+wsport = 9001;
+
+mqtt_topic = 'sitm/http';
---
+++ events/technogalactic-observatory/observation-browser-tool/js/main.js
@@ -0,0 +1,70 @@
+var sitmclient = {
+    'client': null,
+    'lastMessageId': 1,
+    'lastSubId': 1,
+    'subscriptions': [],
+    'messages': [],
+    'observing': false,
+
+    'init' : function() {
+      console.log("sitm::init");
+    },
+
+    'connect': function () {
+        this.client = new Messaging.Client(wsbroker, wsport, "galactiquebrowser_" + parseInt(Math.random() * 100, 10));
+        this.client.onConnectionLost = this.onConnectionLost;
+        this.client.onMessageArrived = this.onMessageArrived;
+
+        var options = {
+          timeout: 3,
+          onSuccess: this.onConnected,
+          onFailure: this.onFailedConnection
+        };
+
+        this.client.connect( options );
+    }, // connect
+
+    'onConnected': function() {
+      console.log("connected to sitm");
+      console.log("subscribing to " + mqtt_topic);
+      sitmclient.client.subscribe(mqtt_topic, {qos: 0});
+      sitmclient.observing = true;
+      $('a#btnStart').html('Stop observation');
+      $('a#btnStart').toggleClass("alert");
+    },
+
+    'onFailedConnection': function() {
+      console.log("Connection failed: " + message.errorMessage);
+      sitmclient.observing = false;
+    },
+
+    'start': function() {
+      console.log("Stating observation");
+      this.connect();
+    },
+
+    'stop': function() {
+      console.log("Stopping observation");
+      this.client.disconnect();
+      this.observing = false;
+      $('a#btnStart').html('Start observation');
+      $('a#btnStart').toggleClass("alert");
+    },
+
+    'toggle': function() {
+      if(this.observing) {
+        this.stop();
+      } else {
+        this.start();
+      }
+    }, // toggle
+
+    'onConnectionLost': function (responseObject) {
+      console.log("connection lost: " + responseObject.errorMessage);
+    },
+
+    'onMessageArrived': function (message) {
+      console.log("<< " + message.payloadString);
+      $('div#log').append(message.payloadString + '<br/>');
+    },
+};
