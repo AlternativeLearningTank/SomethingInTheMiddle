83522b31ef678dc75dbe0e737d466bb094e0c697
Luis Fernandez <dropmeaword@gmail.com>
2017-12-06 22:39:38 +0100

    fixed formatting

--- src/intercept/wifi.py
+++ src/intercept/wifi.py
@@ -33,7 +33,7 @@ class WifiBeacon:
         beacon.bssid      = pkt[Dot11].addr3
         beacon.mac        = pkt.addr2
         beacon.channel    = int( ord(pkt[Dot11Elt:3].info))
-	#print("<<>> " + pkt[Dot11Elt].info + " " + pkt[Dot11].addr3)
+    #print("<<>> " + pkt[Dot11Elt].info + " " + pkt[Dot11].addr3)
         return beacon
 
 
@@ -107,13 +107,13 @@ class WifiProbeScanner:
         # we are looking for management frames with a probe subtype
         # if neither match we are done here
         if pkt.haslayer(Dot11) and (pkt.type == 0):
-           if (pkt.subtype == 0x04):
-               probe = WifiProbe.parse(pkt)
-               # do we already have aprobe for that ssid?
-               if not self.probes.has_key(probe.mac):
-                   self.probes.update( { probe.mac : probe } )
-                   probe.publish()
-            if (pkt.subtype == 0x08):
+            if (pkt.subtype == 0x04):
+                probe = WifiProbe.parse(pkt)
+                # do we already have aprobe for that ssid?
+                if not self.probes.has_key(probe.mac):
+                    self.probes.update( { probe.mac : probe } )
+                    probe.publish()
+            elif (pkt.subtype == 0x08):
                 beacon = WifiBeacon.parse(pkt)
                 # do we already have aprobe for that ssid?
                 if not self.beacons.has_key(beacon.ssid):
